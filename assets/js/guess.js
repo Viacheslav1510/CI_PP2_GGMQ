13ff59c8fcf465803f3cd33c93faa51b23b11580
slava_kyiv <vyacheslavkondriyanenko@gmail.com>
2023-01-09 21:35:45 +0000

    Add guess.js main description

---
+++ assets/js/guess.js
@@ -0,0 +1,5 @@
+/*
+* The guess.js is used by the index.html file to make guess number game work
+* The file generate random number and gets input user's number to check is it compares to each other
+* guess.js keeps high score and gives possibility to refresh the page
+*/
