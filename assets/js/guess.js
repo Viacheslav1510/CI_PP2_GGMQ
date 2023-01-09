225108b2b58b7e5e2e75113e0c763e328a28591e
slava_kyiv <vyacheslavkondriyanenko@gmail.com>
2023-01-09 21:46:04 +0000

    Add DOMContentLoaded event listener and event listeners for check button

--- assets/js/guess.js
+++ assets/js/guess.js
@@ -3,3 +3,34 @@
 * The file generate random number and gets input user's number to check is it compares to each other
 * guess.js keeps high score and gives possibility to refresh the page
 */
+
+// Create all global variables
+let guessBtn = document.querySelector("#check");
+let resetBtn = document.querySelector("#reset");
+let number = Math.floor(Math.random()*10) + 1;
+console.log(number);
+let guessInput = document.getElementById('inputNumber');
+let highestScore = document.getElementById('high-score');
+let message = document.getElementById('message');
+let gameImage = document.getElementById('image');
+let currentScoreElement = document.getElementById('cur-score');
+let atemptsLeftElement = document.getElementById('attempts');
+
+let curScore = 4;
+let attemptsLeft = 4;
+let highScore = 0;
+
+/**
+ * Add Event Listener "DOMContentLoaded" to verify whether a webpage has fully loaded.
+ * Inside function passed event listener for "click" or press "Enter" for check input guess button.
+ */
+document.addEventListener("DOMContentLoaded", function() {
+	
+    guessBtn.addEventListener('click', checkAnswer);
+    
+    guessInput.addEventListener('keydown', function(event) {
+      if (event.key === "Enter") {
+        checkAnswer();
+	  };
+    })
+});
