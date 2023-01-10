775bf963a945910190abee3c04f5fbe04963ce1a
slava_kyiv <vyacheslavkondriyanenko@gmail.com>
2023-01-10 09:31:25 +0000

    Add quiz data in file

---
+++ assets/js/quiz_data.js
@@ -0,0 +1,102 @@
+const quiz = [
+    {
+      question: "What is the derivative of Acceleration with respect to time?",
+      correct_answer: "Jerk",
+      answers: [
+        "Shift",
+        "Bump",
+        "Jerk",
+        "Slide"
+      ]
+    },
+    {
+      question: "What is the area of a circle with a diameter of 20 inches if &pi;= 3.1415?",
+      correct_answer: "314.15 Inches",
+      answers: [
+        "380.1215 Inches",
+        "3141.5 Inches",
+        "1256.6 Inches",
+        "314.15 Inches",
+      ]
+    },
+    {
+      question: "To the nearest whole number, how many radians are in a whole circle?",
+      correct_answer: "6",
+      answers: [
+        "6",
+        "3",
+        "4",
+        "5"
+      ]
+    },
+    {
+      question: "What is the symbol for Displacement?",
+      correct_answer: "Delta",
+      answers: [
+        "dr",
+        "Dp",
+        "Delta",
+        "r",
+      ]
+    },
+    {
+      question: "How many sides does a heptagon have?",
+      correct_answer: "7",
+      answers: [
+        "8",
+        "7",
+        "6",
+        "5"
+      ]
+    },
+    {
+      question: "What is the plane curve proposed by Descartes to challenge Fermat&#039;s extremum-finding techniques called?",
+      correct_answer: "Folium of Descartes",
+      answers: [
+        "Elliptic Paraboloid of Descartes",
+        "Cartesian Coordinates",
+        "Descarte&#039;s Helicoid",
+        "Folium of Descartes",
+      ]
+    },
+    {
+      question: "What is the correct order of operations for solving equations?",
+      correct_answer: "Parentheses, Exponents, Multiplication, Division, Addition, Subtraction",
+      answers: [
+        "Parentheses, Exponents, Multiplication, Division, Addition, Subtraction",
+        "Addition, Multiplication, Division, Subtraction, Addition, Parentheses",
+        "Parentheses, Exponents, Addition, Substraction, Multiplication, Division",
+        "The order in which the operations are written."
+      ]
+    },
+    {
+      question: "Which of the following dice is not a platonic solid?",
+      correct_answer: "10-sided die",
+      answers: [
+        "10-sided die",
+        "12-sided die",
+        "20-sided die",
+        "8-sided die"
+      ]
+    },
+    {
+      question: "Which of the following mathematicians made major contributions to game theory?",
+      correct_answer: "John Von Neumann",
+      answers: [
+        "Carl Friedrich Gauss",
+        "John Von Neumann",
+        "Leonhard Euler",
+        "Stefan Banach"
+      ]
+    },
+    {
+      question: "Which mathematician refused the Fields Medal?",
+      correct_answer: "Grigori Perelman",
+      answers: [
+        "Andrew Wiles",
+        "Terence Tao",
+        "Edward Witten",
+        "Grigori Perelman",
+      ]
+    }
+]
