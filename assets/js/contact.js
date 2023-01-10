8aed2a775d3186937ad3d8cc0912f47314b0e2a8
slava_kyiv <vyacheslavkondriyanenko@gmail.com>
2023-01-10 10:52:13 +0000

    Add script for EmailJs in contact.html and explanation for event listener

---
+++ assets/js/contact.js
@@ -0,0 +1,6 @@
+/**
+ * Add an eventListener to listen for the submit.
+ * Sends an email to site owner through emailJS if the submit is fired.
+ * Script taken from the official EmailJS tutorial https://www.emailjs.com/docs/tutorial/creating-contact-form/ 
+ * and Email Templates Playground environment.
+ */
