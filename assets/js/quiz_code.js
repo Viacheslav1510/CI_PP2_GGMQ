b6fdb70fd9e3a6b5d9fc49d1885624859530f5b9
slava_kyiv <vyacheslavkondriyanenko@gmail.com>
2023-01-10 09:56:04 +0000

    Add showQuestion functionality

--- assets/js/quiz_code.js
+++ assets/js/quiz_code.js
@@ -9,11 +9,23 @@ let currentScoreQuiz = document.getElementById('cur-score');
 let progressBar = document.getElementById('progress-line');
 let quizGameContainer = document.querySelector('.quiz-game');
 
-
+showQuestion();
 /** showQuestion function gets data from "const quiz" and shows question with questionIndex variable.
  * This function uses the loop to show answers 
  * 
  */
 function showQuestion() {
-    
+    questionContainer.innerHTML = quiz[questionIndex].question;
+
+    let answer = '';
+    for (key of quiz[questionIndex].answers ) {
+        answer += `<li>
+                      <label>
+                        <input data-answer="${key}" type="radio" name="answer" class="answer">
+                        <span>${key}</span>
+                      </label>
+                   </li>`;    
+    }
+
+    answersContainer.innerHTML = answer;
 }
