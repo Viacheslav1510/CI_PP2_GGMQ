f4ef08538d142a86c4eae9bcb60e58e0a827c0eb
slava_kyiv <vyacheslavkondriyanenko@gmail.com>
2023-01-10 09:32:29 +0000

    Set all global variables in quiz_code.js

---
+++ assets/js/quiz_code.js
@@ -0,0 +1,10 @@
+let score = 0;
+let questionIndex = 0;
+let percentage = 0;
+let shuffledQuestions;
+let questionContainer = document.getElementById('question');
+let answersContainer = document.getElementById('answer');
+let submitBtn = document.getElementById('submit');
+let currentScoreQuiz = document.getElementById('cur-score');
+let progressBar = document.getElementById('progress-line');
+let quizGameContainer = document.querySelector('.quiz-game');
