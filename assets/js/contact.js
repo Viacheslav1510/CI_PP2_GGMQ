a3debc80a8dcaa2e3b8b9fd26e470defc5f613ed
slava_kyiv <vyacheslavkondriyanenko@gmail.com>
2023-01-10 10:54:37 +0000

    Add EmailJS script to send user's message

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -4,3 +4,16 @@
  * Script taken from the official EmailJS tutorial https://www.emailjs.com/docs/tutorial/creating-contact-form/ 
  * and Email Templates Playground environment.
  */
+document.querySelector() ('.contact-form').addEventListener("submit", function(event) { 
+    event.preventDefault();
+    emailjs.send("service_9ivf2es","template_3jzkvro",{
+        from_name: document.getElementById("name").value,
+        from_email: document.getElementById("email").value,
+        message: document.getElementById("message-form").value,
+        })
+        .then(() => {
+            console.log();
+        }, (err) => {
+            alert("Try again, please.");
+        }); 
+})
