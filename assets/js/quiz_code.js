661a0e9603f452c4873c00763e04504dc76913c4
slava_kyiv <vyacheslavkondriyanenko@gmail.com>
2023-01-10 10:06:30 +0000

    Add checkAnswer function statements for it's functionality

--- assets/js/quiz_code.js
+++ assets/js/quiz_code.js
@@ -10,9 +10,11 @@ let progressBar = document.getElementById('progress-line');
 let quizGameContainer = document.querySelector('.quiz-game');
 
 showQuestion();
-/** showQuestion function gets data from "const quiz" and shows question with questionIndex variable.
- * This function uses the loop to show answers 
- * 
+
+
+/** 
+ * showQuestion function gets data from "const quiz" and shows question with questionIndex variable.
+ * This function uses the loop to show answers. 
  */
 function showQuestion() {
     questionContainer.innerHTML = quiz[questionIndex].question;
@@ -28,4 +30,35 @@ function showQuestion() {
     }
 
     answersContainer.innerHTML = answer;
+}
+
+/**
+ *  checkAnswer function uses checked radio button to compare user's answer with correct answer.
+ *  it increases question index and update progress bar with each submit.
+ *  function increases score with every matched answer.
+ *  function shows alert when user didn't select answer. 
+ */
+function checkAnswer() {
+    let selectedAnswer = answersContainer.querySelector('input[type="radio"]:checked');
+
+    if (!selectedAnswer){
+        alert("Please select answer");
+        return;
+    }
+
+    let userAnswer = selectedAnswer.getAttribute("data-answer");
+    let correctAnswer = quiz[questionIndex].correct_answer;
+    
+    if (userAnswer === correctAnswer){
+        score++;
+        currentScoreQuiz.innerHTML = score;
+    }
+
+    if (questionIndex !== quiz.length - 1) {
+        questionIndex++;
+        showQuestion();
+    } else {
+        
+    }
+
 }
